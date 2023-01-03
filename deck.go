8feaddfabb1e182d31c9db1780102093f517ef4d
Rakshit Gondwal <rakshitgondwal3@gmail.com>
2023-01-03 21:36:00 +0530

    newDeck func done

--- deck.go
+++ deck.go
@@ -4,8 +4,23 @@ import "fmt"
 
 type deck [] string 
 
+func newDeck() deck{
+	cards := []string{}
+
+	cardSuits:= deck{"Spades", "Hearts", "Diamonds"}
+	cardValues:= deck{"Ace","Two","Three"}
+
+	for _,suit:= range cardSuits{
+		for	_,value:= range cardValues{
+			cards = append(cards, value +" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print(){
-	for i,card := range d{
-		fmt.Println(i, card)
+	for _,card := range d{
+		fmt.Println(card)
 	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 
 func main(){
-	cards := deck{newCard(), "Five of Spades", "Three of Hearts"}
-	cards = append(cards, "King of Diamond")
+	cards := newDeck()
 
 	cards.print();
 }
