3cbfb1f61c0faec2f52031e3f475ef5f9a705a51
Rakshit Gondwal <rakshitgondwal3@gmail.com>
2023-01-03 21:15:11 +0530

    add deck.go

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
 
 func main(){
-	fmt.Println("Setup")
+	cards := deck{newCard(), "Five of Spades", "Three of Hearts"}
+	cards = append(cards, "King of Diamond")
+
+	cards.print();
+}
+
+func newCard() string{
+	return "Ace Of Diamonds"
 }
